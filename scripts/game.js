f9bd7d3356c2ccfc92f7ec5f93ed6769975a97ca
Lucas <lucas.ribeiro711@gmail.com>
2021-11-13 23:56:43 -0300

    feat: chang homescreen to game screen

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,11 @@
-console.log('Olá, esta funcionando');
-
+console.log('Olá, esta funcionando!');
+// Sprite 
 const sprites = new Image();
 sprites.src = '../sprites/sprites.png';
-
+// Selecionando o canvas
 const canvas = document.querySelector('canvas');
 const context = canvas.getContext('2d');
-
+// Plano de Fundo
 const background = {
   spriteX:390,
   spriteY:0,
@@ -34,7 +34,7 @@ const background = {
     );
   }
 }
-
+//Chão
 const ground = {
   spriteX:0,
   spriteY:610,
@@ -60,7 +60,7 @@ const ground = {
     );
   }
 }
-
+// Passaro
 const flappyBird = {
   spriteX:0,
   spriteY:0,
@@ -68,6 +68,12 @@ const flappyBird = {
   height:24,
   x:10,
   y:50,
+  gravity: 0.25,
+  speed: 0,
+  refresh() {
+    flappyBird.speed = flappyBird.speed + flappyBird.gravity
+    flappyBird.y = flappyBird.y + flappyBird.speed;
+  },
   draw(){
     context.drawImage(
       sprites,
@@ -78,13 +84,69 @@ const flappyBird = {
     );
   }
 }
+//Tela de inicio
+const homeScreen = {
+  spriteX:134,
+  spriteY:0,
+  width:147,
+  height:152,
+  x:(canvas.width / 2) -174 /2 ,
+  y:50,
+  draw(){
+    context.drawImage(
+      sprites,
+      homeScreen.spriteX, homeScreen.spriteY,
+      homeScreen.width, homeScreen.height,
+      homeScreen.x, homeScreen.y,
+      homeScreen.width, homeScreen.height,
+    );
+  }
+}
+
+//Telas 
+let activeScreen = {}
+function changeScreen(newScreen) {
+  activeScreen = newScreen
+}
+const screens = {
+  START: {
+    draw(){
+      background.draw()
+      ground.draw()
+      flappyBird.draw()
+      homeScreen.draw()
+    },
+    click(){
+      changeScreen(screens.GAME)
+    },
+    refresh(){
+
+    }
+  }
+}
+
+screens.GAME = {
+  draw(){
+    background.draw()
+    ground.draw()
+    flappyBird.draw()
+  },
+  refresh(){
+    flappyBird.refresh()
+  }
+}
+
+
 function loop() {
-  background.draw()
-  ground.draw()
-  flappyBird.draw()
-
-
+  activeScreen.draw()
+  activeScreen.refresh()
   requestAnimationFrame(loop)
 }
 
+window.addEventListener('click', function(){
+  if(activeScreen.click) {
+    activeScreen.click();
+  }
+})
+changeScreen(screens.START)
 loop();
